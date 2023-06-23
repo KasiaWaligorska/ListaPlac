b6f583be4444618abe5dc1ef3e3b79ceda757ca2
Kejtbusz <iwonaantonina90@gmail.com>
2023-06-23 14:12:01 +0200

    initial

---
+++ ListaPłac/Program.cs
@@ -0,0 +1,34 @@
+﻿using ListaPłac;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Company company = new Company("BigCompany");
+        Menu menu = new Menu(company);
+         Employee employee;
+        
+         int numberOfEmployees = 2;
+         for (int i = 1; i <= numberOfEmployees; i++)
+         {
+             employee = new Employee();
+        
+             try
+             {
+                 employee.CreateNewEmployee();
+             }
+             catch (ArgumentNullException e)
+             {
+                 Console.WriteLine("Null is not allowed.");
+             }
+             catch (ArgumentException e)
+             {
+                 Console.WriteLine("Value must be bigger than 0");
+             }
+        
+             company.AddEmployeeToEmployeesList(employee);
+         }
+        
+        menu.PerformAnActionFromTheMenu();
+    }
+}
